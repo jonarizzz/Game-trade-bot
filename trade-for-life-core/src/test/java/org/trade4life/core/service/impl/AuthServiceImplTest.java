517cfe3c5896d31063cb8360a8928649eb14f800
Yauheni Azaronak <jonarizzzz@gmail.com>
2023-10-31 17:14:11 +0100

    Auth service is covered

--- trade-for-life-core/src/test/java/org/trade4life/core/service/impl/AuthServiceImplTest.java
+++ trade-for-life-core/src/test/java/org/trade4life/core/service/impl/AuthServiceImplTest.java
@@ -2,6 +2,7 @@ package org.trade4life.core.service.impl;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -40,8 +41,7 @@ class AuthServiceImplTest {
 
     @Test
     public void loginShouldCreateUserIfNotPresent() {
-
-        when(userService.getUserByTelegramId(any())).thenReturn(Optional.empty());
+        when(userService.getUserByTelegramId(eq(USER_TELEGRAM_ID))).thenReturn(Optional.empty());
         when(userMapper.toModel(any())).thenReturn(userModel());
         when(userService.saveUser(any())).thenReturn(userModel());
         when(userMapper.toDto(any())).thenReturn(userLoginResponseDto(USER_ID, null));
@@ -59,7 +59,38 @@ class AuthServiceImplTest {
 
     @Test
     public void loginShouldGetExistingUserIfExists() {
-        when(userService.getUserByTelegramId(any())).thenReturn(Optional.of(userModelWithoutRegion()));
+        when(userService.getUserByTelegramId(eq(USER_TELEGRAM_ID))).thenReturn(Optional.of(userModel()));
+        when(userMapper.toDto(any())).thenReturn(userLoginResponseDto());
+
+        UserLoginResponseDto loginResult = authService.login(userLoginRequestDto());
+
+        verify(userService).getUserByTelegramId(eq(USER_TELEGRAM_ID));
+        verify(userMapper).toDto(any(UserModel.class));
+
+        assertThat(loginResult.getUserId()).isEqualTo(USER_ID);
+        assertThat(loginResult.getIsNew()).isEqualTo(false);
+    }
+
+    @Test
+    public void loginShouldUpdateUserNicknameIfChanged() {
+        String oldUserNickname = "OldNickname";
+
+        when(userService.getUserByTelegramId(eq(USER_TELEGRAM_ID)))
+            .thenReturn(Optional.of(userModel(USER_ID, USER_TELEGRAM_ID, oldUserNickname, USER_NAME, USER_PHONE, USER_BIO)));
+        when(userService.saveUser(any())).thenReturn(userModel());
+        when(userMapper.toDto(any())).thenReturn(userLoginResponseDto());
+        ArgumentCaptor<UserModel> userModelCaptor = ArgumentCaptor.forClass(UserModel.class);
+
+        authService.login(userLoginRequestDto());
+
+        verify(userService).saveUser(userModelCaptor.capture());
+        UserModel capturedUserModel = userModelCaptor.getValue();
+        assertThat(capturedUserModel.getNickname()).isEqualTo(USER_NICKNAME);
+    }
+
+    @Test
+    public void loginResultShouldSayNewUserIfUserGotNoRegion() {
+        when(userService.getUserByTelegramId(eq(USER_TELEGRAM_ID))).thenReturn(Optional.of(userModelWithoutRegion()));
         when(userMapper.toDto(any())).thenReturn(userLoginResponseDto(USER_ID, null));
 
         UserLoginResponseDto loginResult = authService.login(userLoginRequestDto());
@@ -69,15 +100,6 @@ class AuthServiceImplTest {
 
         assertThat(loginResult.getUserId()).isEqualTo(USER_ID);
         assertThat(loginResult.getIsNew()).isEqualTo(true);
-
-    }
-
-    public void loginShouldUpdateUserNicknameIfChanged() {
-
-    }
-
-    public void loginResultShouldSayNewUserIfUserGotNoRegion() {
-
     }
 
 }
